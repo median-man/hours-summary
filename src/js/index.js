bf85ae6cad940a7a369ab40da9db7d81f2c0bef8
John Desrosiers <john.desro@gmail.com>
2019-12-05 09:58:17 -0800

    Remove template code

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,33 @@
 import 'bootstrap';
-
 import '../scss/index.scss';
+import * as auth from './auth';
 
-$('#alert').click(() => {
-  alert('jQuery works!');
+let $loginButton;
+
+function updateSignInStatus(isSignedIn) {
+  if (isSignedIn) {
+    $loginButton.text('Log Out').toggleClass('');
+  } else {
+    $loginButton.text('Sign In with Google');
+  }
+}
+
+function handleLoginClick() {
+  try {
+    if (auth.isSignedIn()) {
+      auth.signOut();
+      return;
+    }
+    $loginButton.text('Loading...');
+    auth.signIn();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+$(() => {
+  $loginButton = $('#button-login');
+  $loginButton.text('Loading...');
+  $loginButton.on('click', handleLoginClick);
+  auth.init({ onChange: updateSignInStatus });
 });
-
-// Your jQuery code
