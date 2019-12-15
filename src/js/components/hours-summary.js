fcb1290f74beb7da7f1f7a0f921b406e6d872938
John Desrosiers <john.desro@gmail.com>
2019-12-15 08:57:25 -0800

    Add hours last week to summary

--- src/js/components/hours-summary.js
+++ src/js/components/hours-summary.js
@@ -1,35 +1,44 @@
+const weekDays = [
+  'sunday',
+  'monday',
+  'tuesday',
+  'wednesday',
+  'thursday',
+  'friday',
+  'saturday',
+];
+
 export default class HoursSummary {
   constructor({ hours }) {
     this._hours = hours;
   }
   render() {
-    const { currentDay, currentWeek } = this._hours;
+    const { currentWeek, previousWeek } = this._hours;
 
     return [
-      `<p class="h4">Hours Today: ${currentDay.hours.toFixed(2)}</p>`,
-      `<p class="h4 pt-3">Hours This Week: ${currentWeek.hours.toFixed(2)}</p>`,
+      `<p class="h5 pt-3">${currentWeek.hours.toFixed(2)} hours this week</p>`,
+      `<p class="pt-3">${previousWeek.hours.toFixed(2)} hours last week</p>`,
       this.renderDaysTable(),
     ].join('');
   }
 
   renderDaysTable() {
-    const rows = [
-      'sunday',
-      'monday',
-      'tuesday',
-      'wednesday',
-      'thursday',
-      'friday',
-      'saturday',
-    ]
-      .map(day => this.renderDaysTableRow(day))
+    const today = new Date().getDay();
+    const isToday = dayIndex => dayIndex === today;
+    const rows = weekDays
+      .map((day, index) => this.renderDaysTableRow(day, isToday(index)))
       .join('');
     return `<table class="table mt-5" style="max-width: 400px"><tbody>${rows}</tbody></table>`;
   }
 
-  renderDaysTableRow(day) {
-    const th = `<th>${day[0].toUpperCase() + day.substr(1)}</th>`;
+  renderDaysTableRow(day, isToday) {
     const td = `<td>${this._hours[day].hours.toFixed(2)}</td>`;
+    if (isToday) {
+      const badge = `<span class="badge badge-primary">Today</span>`;
+      const th = `<th>${day[0].toUpperCase() + day.substr(1)} ${badge}</th>`;
+      return `<tr class="table-primary">${th}${td}</tr>`;
+    }
+    const th = `<th>${day[0].toUpperCase() + day.substr(1)}</th>`;
     return `<tr>${th}${td}</tr>`;
   }
 }
