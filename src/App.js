bb2eb17041c3b24a9fb0e8dd391e42fee496c0c5
John Desrosiers <john.desro@gmail.com>
2020-01-24 08:07:52 -0800

    WIP: inital SheetList component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import SheetList from "./pages/SheetList";
 
 function App() {
   return (
   <Router>
     <Navbar />
     <Route exact path="/">
-      App
+      <SheetList />
     </Route>
   </Router>
   );
---
+++ src/pages/SheetList/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SheetList = () => {
+  return <div>Sheet List</div>;
+};
+
+export default SheetList;
