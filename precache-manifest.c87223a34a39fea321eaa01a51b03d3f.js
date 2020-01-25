d706f0ebbeaad5d9ea5f246180608bd37b3a0a6d
John Desrosiers <john.desro@gmail.com>
2020-01-25 13:35:59 -0800

    Updates

---
+++ precache-manifest.c87223a34a39fea321eaa01a51b03d3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681e108d61d7c5b3de0617129017d08a",
+    "url": "/hours-summary/index.html"
+  },
+  {
+    "revision": "2d7893115a99bfadb183",
+    "url": "/hours-summary/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "252e7d794b3cfe6e3962",
+    "url": "/hours-summary/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2d7893115a99bfadb183",
+    "url": "/hours-summary/static/js/2.8372a00f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hours-summary/static/js/2.8372a00f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "252e7d794b3cfe6e3962",
+    "url": "/hours-summary/static/js/main.71e5d40c.chunk.js"
+  },
+  {
+    "revision": "242b4e3f0694543881fa",
+    "url": "/hours-summary/static/js/runtime-main.1c31b21a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a974a9480244903b9cd921eda1c698e9.js"
+  "/hours-summary/precache-manifest.c87223a34a39fea321eaa01a51b03d3f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hours-summary/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
