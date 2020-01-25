22e46c457782d79c6bc13e84901524e158ae7358
John Desrosiers <john.desro@gmail.com>
2020-01-25 13:33:40 -0800

    Add error screen for gapi load failure

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ import Spinner from "./components/Spinner";
 
 function App() {
   const [isGapiClientLoaded, setIsGapiClientLoaded] = useState(false);
+  const [gapiError, setGapiError] = useState(null);
   useEffect(() => {
     gapi
       .loadClientWithAuth()
       .then(() => setIsGapiClientLoaded(true))
       .catch(error => {
-        console.log(error);
         setIsGapiClientLoaded(false);
+        setGapiError(error)
       });
   }, []);
 
@@ -26,6 +27,20 @@ function App() {
         <Navbar />
         <Dashboard hours={new Hours({ sheetsApi })} />
       </AuthProvider>
+    );
+  }
+
+  if (gapiError) {
+    return (
+      <div className="vh-100 d-flex justify-content-center align-items-center bg-light">
+        <div className="text-danger">
+          <h1 className="display-3">Uh, oh!</h1>
+          <p>
+            An unexpected error occurred while connecting with Google. Please
+            try again later.
+          </p>
+        </div>
+      </div>
     );
   }
   return <Spinner />;
