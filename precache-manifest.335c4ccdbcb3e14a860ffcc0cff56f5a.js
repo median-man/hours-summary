2d0ab3e61532d07b3568f105db8333ec60d86bf9
John Desrosiers <john.desro@gmail.com>
2020-01-25 13:06:41 -0800

    Updates

---
+++ precache-manifest.335c4ccdbcb3e14a860ffcc0cff56f5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bb052bdead35a6df4ebb690bf24ea0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d7893115a99bfadb183",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3ea0e7ae4a311579d440",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "2d7893115a99bfadb183",
+    "url": "/static/js/2.8372a00f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8372a00f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ea0e7ae4a311579d440",
+    "url": "/static/js/main.c6eccde7.chunk.js"
+  },
+  {
+    "revision": "f3e56ebaa949583789dd",
+    "url": "/static/js/runtime-main.2ca56e04.js"
+  }
+]);
