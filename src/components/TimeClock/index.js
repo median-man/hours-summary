15d2ea0e0358b11f740323d3cf8584277e27c135
John Desrosiers <john.desro@gmail.com>
2020-01-26 05:39:06 -0800

    TimeClock renders buttons and remarks input

--- src/components/TimeClock/index.js
+++ src/components/TimeClock/index.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Container from "../Container";
+
+const Button = props => (
+  <button type="button" className="btn btn-outline-dark btn-block" {...props} />
+);
 
 const TimeClock = () => {
-    return <div>TimeClock</div>
+  return (
+    <Container>
+      <div style={{ maxWidth: "400px" }}>
+        <h1 className="h2 pb-3">Time Clock</h1>
+        <Button>In</Button>
+        <Button>Out</Button>
+        <div className="form-group mt-3">
+          <label htmlFor="remarks">Remarks</label>
+          <textarea
+            className="form-control"
+            id="remarks"
+            rows={3}
+            defaultValue={""}
+          />
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default TimeClock;
